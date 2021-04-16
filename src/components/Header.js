c63d483da233482d7b07ec72d0c111ea857ef0df
Juan D. Jara <juanorigami@gmail.com>
2021-04-16 02:26:18 +0200

    home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { useSiteData } from 'react-static'
+import { Link } from '@reach/router'
 
 export default function Header ({ className = '', ...props }) {
   const { title, description } = useSiteData()
   return (
     <header className={`text-white flex justify-between items-start ${className}`} {...props}>
-      <div className="flex p-2">
+      <Link to='/' className="flex p-2">
         <img className="mr-2 w-20" src="/images/escudo-fullcolor.png" alt="Escudo guardianes" />
         <div>
           <h1 className="text-2xl mt-2">{title}</h1>
           <p>{description}</p>
         </div>
-      </div>
+      </Link>
       <nav className="flex items-start p-3 space-x-3">
         <a className="rounded-md py-2 px-4 hover:bg-red-600 bg-red-500" href="/blog">Blog</a>
         <a className="rounded-md py-2 px-4 hover:bg-red-600 bg-red-500" href="/dossier">Dossier</a>
