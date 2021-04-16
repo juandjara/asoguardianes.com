49ff785e2a9e5fff3c36b3e708a15db0f433e9ee
Juan D. Jara <juanorigami@gmail.com>
2021-04-16 11:56:42 +0200

    add placeholders

---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "components/Header";
+
+export default function Contact() {
+  return (
+    <div>
+      <Header></Header>
+      <div>
+        <h1 className="text-4xl">Contacto</h1>
+      </div>
+    </div>
+  )
+}
