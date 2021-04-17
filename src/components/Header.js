5fc025a80f26492f4304fc2912f18278907443c5
Juan D. Jara <juanorigami@gmail.com>
2021-04-17 14:15:24 +0200

    correct grid spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { Link } from '@reach/router'
 export default function Header ({ className = '', ...props }) {
   const { title, description } = useSiteData()
   return (
-    <header className={`text-white flex justify-between items-start ${className}`} {...props}>
+    <header className={`text-white flex flex-wrap justify-between items-start ${className}`} {...props}>
       <Link to='/' className="flex p-2">
         <img className="mr-2 w-20" src="/images/escudo-fullcolor.png" alt="Escudo guardianes" />
         <div>
@@ -13,11 +13,11 @@ export default function Header ({ className = '', ...props }) {
           <p>{description}</p>
         </div>
       </Link>
-      <nav className="flex items-start p-3 space-x-3">
-        <a className="rounded-md py-2 px-4 hover:bg-red-600 bg-red-500" href="/blog">Blog</a>
-        <a className="rounded-md py-2 px-4 hover:bg-red-600 bg-red-500" href="/dossier">Dossier</a>
-        <a className="rounded-md py-2 px-4 hover:bg-red-600 bg-red-500" href="/calendario">Calendario</a>
-        <a className="rounded-md py-2 px-4 hover:bg-red-600 bg-red-500" href="/contacto">Contacto</a>
+      <nav className="flex flex-wrap items-start pt-0 p-3 space-x-3">
+        <a className="rounded-md mt-2 py-2 px-4 hover:bg-red-600 bg-red-500" href="/blog">Blog</a>
+        <a className="rounded-md mt-2 py-2 px-4 hover:bg-red-600 bg-red-500" href="/dossier">Dossier</a>
+        <a className="rounded-md mt-2 py-2 px-4 hover:bg-red-600 bg-red-500" href="/calendario">Calendario</a>
+        <a className="rounded-md mt-2 py-2 px-4 hover:bg-red-600 bg-red-500" href="/contacto">Contacto</a>
       </nav>
     </header>
   )
--- src/pages/Dossier.js
+++ src/pages/Dossier.js
@@ -3,21 +3,45 @@ import Header from "components/Header";
 import GFM from 'remark-gfm'
 import Markdown from 'react-markdown'
 import { useRouteData } from 'react-static'
+import styled from 'styled-components'
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-areas:
+    'head head head'
+    'nav main nav2'
+    'foot foot foot';
+
+  .header-area { grid-area: head; }
+  .foot-area { grid-area: foot; }
+  .nav-area {
+    grid-area: nav;
+  }
+  .nav2-area {
+    grid-area: nav2;
+  }
+  .main-area {
+    grid-column: span 3 / span 3;
+    @media (min-width: 768px) {
+      grid-area: main;
+    }
+  }
+`
 
 export default function Dossier() {
-  const { page, dossierPages } = useRouteData()  
+  const { page, dossierPages } = useRouteData()
   return (
-    <div className="grid grid-cols-3 auto-cols-min auto-rows-min">
-      <Header className="col-span-3 bg-red-700" />
-      <aside></aside>
-      <main>
+    <Grid>
+      <Header className="header-area bg-red-700" />
+      <aside className="hidden md:block nav-area"></aside>
+      <main className="main-area px-4">
         <h1 className="pt-8 font-semibold text-center text-4xl mb-4">{page.data.title}</h1>
         <div className="prose prose-red lg:prose-xl mx-auto py-8">
           <Markdown remarkPlugins={[GFM]}>{page.content}</Markdown>
         </div>
       </main>
-      <aside></aside>
-      <footer className="col-span-3"></footer>
-    </div>
+      <aside className="hidden md:block nav2-area"></aside>
+      <footer className="footer-area"></footer>
+    </Grid>
   )
 }
