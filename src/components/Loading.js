5926ebae5901c4f8f72a9acebdc6f6df3c2d9260
Juan D. Jara <juanorigami@gmail.com>
2021-04-19 13:56:17 +0200

    loading styles

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Loading = () => (
   <div className="loading" style={{
-    fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center'
+    fontFamily: 'sans-serif',
+    padding: '2rem',
+    textAlign: 'center',
+    display: 'grid',
+    placeContent: 'center',
+    height: '100vh'
   }}>
-    <p style={{marginBottom: 8, fontSize: '1.4rem'}}>
-      <strong>Asociación Guardianes</strong>
-    </p>
-    <span>Cargando ...</span>
+    <p style={{marginBottom: 8, fontSize: '3rem', color: 'red'}}>Asociación Guardianes</p>
+    <span>Cargando el <em>portal</em> ...</span>
   </div>
 )
 
