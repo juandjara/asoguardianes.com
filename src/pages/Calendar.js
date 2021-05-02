81dc8a4f0b4bad6b58282e3398588f2ef7ff03ea
Juan D. Jara <juanorigami@gmail.com>
2021-05-02 02:44:52 +0200

    tweak calendar layout

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
 export default function Calendar() {
+  const baseURL = 'https://calendar.google.com/calendar/embed'
+  const params = {
+    height: 600,
+    wkst: 2,
+    color: '#D50000',
+    bgcolor: '#ffffff',
+    ctz: 'Europe/Madrid',
+    src: 'YXNvY2lhY2lvbi5ndWFyZGlhbmVzQGdtYWlsLmNvbQ',
+    showPrint: 0,
+    showTitle: 0,
+    showTz: 0,
+    showCalendars: 0,
+    showTabs: 1,
+    title: 'Eventos Guardianes',
+    mode: 'WEEK'
+  }
   return (
-    <div className="max-w-4xl mx-auto">
-      <h1 className="text-4xl py-6 mb-6 font-bold">Calendario de eventos</h1>
-      <div className="relative" style={{ paddingTop: '56.25%' }}>
+    <div className="max-w-4xl mx-auto px-2 mb-2">
+      <h1 className="font-medium text-center text-gray-700 text-4xl py-6 mt-6 mb-2">Calendario de eventos</h1>
+      <div className="relative" style={{ paddingTop: '100%' }}>
         <iframe
           title="Google Calendar asociacion.guardianes@gmail.com"
-          src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FMadrid&amp;src=YXNvY2lhY2lvbi5ndWFyZGlhbmVzQGdtYWlsLmNvbQ&amp;color=%23D50000&amp;showPrint=0&amp;showTitle=0&amp;showTz=0&amp;showCalendars=0&amp;showTabs=1&amp;title=Eventos%20Guardianes"
-          frameborder="0"
+          src={`${baseURL}?${Object.entries(params).map(e => `${e[0]}=${encodeURIComponent(e[1])}`).join('&')}`}
+          frameBorder="0"
           scrolling="no"
-          className="absolute inset-0 w-full h-full"
+          className="mx-auto absolute inset-0 w-full h-full"
         />
       </div>
     </div>
